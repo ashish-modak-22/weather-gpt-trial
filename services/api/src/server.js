4a543eee9c1983b8519a7daae7624ac0297f13c2
Kanchan Nath <kanchannath.webdev@gmail.com>
2026-09-05 22:04:28 +0530

    add: implement server setup with dotenv configuration and MongoDB connection

---
+++ services/api/src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config()
+import {app} from "./app.js"
+import {connectDB} from "./db/db.js"
+
+const PORT = process.env.PORT || 4000
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at port ${process.env.PORT}\n`)
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO DB connection failed !!! \n", error);
+    })
